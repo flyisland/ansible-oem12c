ff3433dc31ccca62866c1a43e2bc8ed455eba7ff
flyisland <flyisland@gmail.com>
2016-04-01 11:26:05 +0800

    minor fix

--- install_ansible.sh
+++ install_ansible.sh
@@ -4,7 +4,7 @@
 sudo mv /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.backup
 sudo curl http://mirrors.163.com/.help/CentOS6-Base-163.repo -o /etc/yum.repos.d/CentOS-Base.repo
 sudo yum clean all
-sudo yum makecache
+# sudo yum makecache
 sudo yum install ansible -y
 
 ansible-playbook /vagrant/setup_ansible.yml
